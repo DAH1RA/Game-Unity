bcca79da73e97ea09d47e62d96523fe7ed2c847d
DA020 <1945027@brunel.ac.uk>
2021-11-23 19:59:36 +0000

    added boss to level3 working on patrol now, making the boss move around

---
+++ Assets/Scripts/Enemy/MeleeEnemy.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+
+public class MeleeEnemy : MonoBehaviour
+{
+    //[Header("Attack Parameters")]
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private float range;
+    [SerializeField] private float colliderDistance;
+    [SerializeField] private int damage;
+    [SerializeField] private BoxCollider2D boxCollider;
+    [SerializeField] private LayerMask playerLayer;
+    private float cooldownTimer = Mathf.Infinity;
+
+    //References
+    private Animator anim;
+    private Health playerHealth;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        
+    }
+
+    private void Update()
+    {
+        cooldownTimer += Time.deltaTime;
+
+        if (PlayerInSight())
+        {
+            if (cooldownTimer >= attackCooldown)
+            {
+                cooldownTimer = 0;
+                anim.SetTrigger("meleeAttack");
+            }
+        }
+
+    }
+
+    /*private void RangedAttack()
+    {
+        cooldownTimer = 0;
+        fireballs[FindFireball()].transform.position = firepoint.position;
+        fireballs[FindFireball()].GetComponent<EnemyProjectile>().ActivateProjectile();
+    }
+    private int FindFireball()
+    {
+        for (int i = 0; i < fireballs.Length; i++)
+        {
+            if (!fireballs[i].activeInHierarchy)
+                return i;
+        }
+        return 0;
+    }*/
+
+    private bool PlayerInSight()
+    {
+        RaycastHit2D hit =
+            Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
+            0, Vector2.left, 0, playerLayer);
+
+        if (hit.collider != null)
+            playerHealth = hit.transform.GetComponent<Health>();
+
+        return hit.collider != null;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
+    }
+
+    private void DamagePlayer()
+    {
+        //if player in range damage player
+        if (PlayerInSight())
+            playerHealth.TakeDamage(damage);
+    }
+    
+}
